b3a9d8baaa0073713820da0afb0a04a1b4f0f6ab
Jonathan Muller <jonathan@cubyn.com>
2017-05-23 14:32:19 +0200

    Remove XPACK, update to 5.4, precompile kibana

--- elasticsearch/start.sh
+++ elasticsearch/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# This is for Kubernetes compat because we cant put env vars
+# having special characters used by elasticsearch ('.')
+# so we transform every env var (duplicate), changing _ to .
+# ELASTIC_MAX_VAR becomes ELASTIC.MAX.VAR
 MEGAENV=''
 while IFS='=' read -r name value ; do
   if [[ $value != *" "* ]]; then
